0414457a4110246c8c904ebc33e50d8fd0877097
Your Name <wamontgomery0068@gmail.com>
2018-12-14 10:43:14 -0600

    Setting up the Server

--- Server/index.js
+++ Server/index.js
@@ -7,8 +7,13 @@ require('dotenv').config()
 const app = express();
 app.use( bodyParser.json() );
 
+const port = 3056;
+
 massive( process.env.CONNECTION_STRING ).then(db => {
     app.set('db', db)
 }).catch( err => console.log(err) );
 
 // ***** RESTful Request with Associated Endpoints below *****
+
+app.listen(port, () => {console.log(`Cowabunga!: ${port}`)
+});
