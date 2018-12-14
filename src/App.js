e7397404b2f504ff96edd80693f9610739de6657
Your Name <wamontgomery0068@gmail.com>
2018-12-14 12:18:28 -0600

    Working on the Form Component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import React, { Component } from 'react';
 import Dashboard from './Components/Dashboard/Dashboard';
-import Form from './Components/Form/Form';
 import Header from './Components/Header/Header';
+import Form from './Components/Form/Form';
 
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div className="App_Container">
         <Dashboard />
-        <Form />
         <Header />
+        <Form />
       </div>
     );
   }
--- src/Components/Form/Form.js
+++ src/Components/Form/Form.js
@@ -6,8 +6,42 @@ import './Form.css';
 class Form extends Component {
     render(){
         return(
-            <div className = "Form">
-                <h1> Form Component </h1>
+            <div className = "Form_Container">
+                <form>
+
+                    <img className="Preview"></img>
+
+                    <br />
+
+                    Image_URL: <input
+                        type = "text"
+                        placeholder = "Add Image Here"
+                        // onChange = {}
+                    />
+
+                    <br />
+
+                    Product Name: <input
+                        type = "text"
+                        placeholder = "Add Name Here"
+                        // onChange = {}
+                    />
+
+                    <br />
+
+                    Price: <input
+                        type = "text"
+                        placeholder = "Add Price Here"
+                        // onChange = {}
+                    />
+
+                    <br />
+
+                    <button className = "Cancel_Button"> Cancel </button>
+
+                    <button className = "Add_Button"> Add to Inventory </button>
+                    
+                </form>
             </div>
         )
     }
